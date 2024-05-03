24383ab013c9dde47bd38864d983fc8ee72785f3
unknown <almymnhassan@gmail.com>
2024-05-03 17:03:08 +0300

    add more important operations

--- necessary/operation.php
+++ necessary/operation.php
@@ -18,8 +18,17 @@ return $result;
 function get10questionwithmostanswers(){
 $db = dbcon();
 
-$sql = "SELECT question.id ,question.title,COUNT(*) AS numofans FROM question ,answer WHERE question.id=answer.questionid 
+$sql = "SELECT question.id ,question.title,question.content,COUNT(*) AS numofans FROM question ,answer WHERE question.id=answer.questionid 
  GROUP BY question.id ORDER BY numofans DESC  LIMIT 10 ;";
+$result = mysqli_query($db, $sql);
+
+return $result;
+
+}
+function getuserquestions($userid){
+$db = dbcon();
+
+$sql = "SELECT title,content FROM stack_user,question WHERE stack_user.id=question.userid AND stack_user.id=".$userid.";";
 $result = mysqli_query($db, $sql);
 
 return $result;
@@ -27,3 +36,12 @@ return $result;
 }
 
 
+function getuseranswers($userid){
+$db = dbcon();
+
+$sql = "SELECT title,content FROM stack_user,answer WHERE stack_user.id=answer.userid AND stack_user.is=".$userid.";";
+$result = mysqli_query($db, $sql);
+
+return $result;
+
+}
