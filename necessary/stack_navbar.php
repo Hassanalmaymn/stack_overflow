e5fb8cca8e840b62bc0768d19736bdb2e72f35f3
unknown <almymnhassan@gmail.com>
2024-05-04 14:04:14 +0300

    changes in navbar

--- necessary/stack_navbar.php
+++ necessary/stack_navbar.php
@@ -8,19 +8,34 @@
                 data-bs-target="#navbarSupportedContent" 
                 aria-controls="navbarSupportedContent"
                 aria-expanded="false" 
+
                 aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+
+
+
+
                 </li>
 
-                <form class="d-flex" method="GET" action="necessary/operation.php" role="search">
+                <form class="d-flex" method="GET" action="index.php" role="search">
                     <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
                 </form>
+
         </div>
+        <?php
+        require_once'User.php';
+        if (!isLoggedIn()) {
+            echo'<a class="btn btn-outline-dark justify-content-end m-2" aria-current="page" href="signin.php">sign in</a>' .
+            '<a class="btn btn-outline-dark justify-content-end m-2" aria-current="page" href="signup.php">sign up</a>';
+        } else {
+            echo'<a class="btn btn-outline-dark justify-content-end m-2" aria-current="page" href="index.php">my questions</a>' .
+            '<a class="btn btn-outline-dark justify-content-end m-2" aria-current="page" href="index.php">my answers</a>';
+        }
+        ?>
     </div>
 </nav>
